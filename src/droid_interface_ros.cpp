9da6508e14380890ffc056e6b307db7c6f21bc72
Yoni <yweiner@wpi.edu>
2021-04-06 00:05:35 -0400

    formatting

--- src/droid_interface_ros.cpp
+++ src/droid_interface_ros.cpp
@@ -3,13 +3,12 @@
  * Serves as the interface between ROS nodes on the main board and motor drivers, etc.
  */
 
-
 #include <Arduino.h>
 #include "ros_ugv.h"
 
 ROSUGV robot;
-   int16_t left = 500;
-   int16_t right = 500;
+int16_t left = 500;
+int16_t right = 500;
 
 //N.B.: No need to start ROS serial manually, as the constructors take care of that for us
 void setup()
@@ -19,11 +18,11 @@ void setup()
   // {
   //   delay(100);
   // }
-  
+
   DEBUG_SERIAL.println("setup");
 
   robot.Init();
- 
+
   DEBUG_SERIAL.println("/setup");
 }
 
@@ -34,25 +33,27 @@ void loop(void)
   if (true)
   {
     // all in m/s, rad/s
-  //  float left = debugString.toFloat();
-  //  uint8_t comma = debugString.indexOf(',');
-  //  float right = debugString.substring(comma+1).toFloat();
+    //  float left = debugString.toFloat();
+    //  uint8_t comma = debugString.indexOf(',');
+    //  float right = debugString.substring(comma+1).toFloat();
 
-  //  DEBUG_SERIAL.print("Setting left = ");
-  //  DEBUG_SERIAL.println(left);
-  //  DEBUG_SERIAL.print("Setting right = ");
-  //  DEBUG_SERIAL.println(right);
-    if(millis() % 4000 > 2000){
-   right = 500;
-   left = 500;
-    } else {
-   left = -500;
-   right = -500;
+    //  DEBUG_SERIAL.print("Setting left = ");
+    //  DEBUG_SERIAL.println(left);
+    //  DEBUG_SERIAL.print("Setting right = ");
+    //  DEBUG_SERIAL.println(right);
+    if (millis() % 4000 > 2000)
+    {
+      right = 500;
+      left = 500;
+    }
+    else
+    {
+      left = -500;
+      right = -500;
     }
 
-   robot.SetTargetPositions(left, right);
- 
+    robot.SetTargetPositions(left, right);
 
-  //  debugString = "";
+    //  debugString = "";
   }
 }
